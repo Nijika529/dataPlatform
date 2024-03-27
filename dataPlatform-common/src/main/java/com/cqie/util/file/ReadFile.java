4693b11c73b18bdb868d2ddbb908f28176215358
lazous <2084384078@qq.com>
2024-03-27 14:45:21 +0800

    初版

--- dataPlatform-common/src/main/java/com/cqie/util/file/ReadFile.java
+++ dataPlatform-common/src/main/java/com/cqie/util/file/ReadFile.java
@@ -2,10 +2,12 @@ package com.cqie.util.file;
 
 import cn.afterturn.easypoi.excel.ExcelImportUtil;
 import cn.afterturn.easypoi.excel.entity.ImportParams;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.poi.ss.formula.functions.T;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.util.CellRangeAddress;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileInputStream;
@@ -20,20 +22,32 @@ import java.util.List;
  * @create: 2024-03-20 13:29
  **/
 
+@Slf4j
 public class ReadFile {
     public static <T> List<T> readWrite(String path, Class<?> clazz) throws Exception {
+        log.info("开始读取表格");
         FileInputStream fileInputStream = new FileInputStream(path);
         ImportParams importParams = new ImportParams();
-
+        //创建xlsx的工作薄
         Workbook workbook = new XSSFWorkbook(Files.newInputStream(Paths.get(path)));
+        //获取第一个表
         Sheet sheet = workbook.getSheetAt(0);
-        for (int i=0;i<sheet.getLastRowNum();i++){
-            Row row = sheet.getRow(i);
-            if(row!=null&&row.getRowStyle()!=null){
-                importParams.setHeadRows(i+1);
+        int headRow = 1;
+        //for循坏获取行数
+        List<CellRangeAddress> mergedRegions = sheet.getMergedRegions();
+        if (!mergedRegions.isEmpty()) {
+            // 遍历合并区域
+            for (CellRangeAddress  mergedRegion:mergedRegions) {
+                int firstRow = mergedRegion.getFirstRow();
+                int lastRow = mergedRegion.getLastRow();
+                int rowCount = lastRow - firstRow + 1;
+                // 如果合并单元格的起始行号小于当前的 headRow，则更新 headRow
+                if (headRow < rowCount) {
+                    headRow = rowCount;
+                }
             }
         }
-
+        importParams.setHeadRows(headRow);
         importParams.setTitleRows(0);
         List<T> res = ExcelImportUtil.importExcel(fileInputStream, clazz, importParams);
         fileInputStream.close();
