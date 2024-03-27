d120de6516150fc0c65f158aae069e483704f77e
lazous <2084384078@qq.com>
2024-03-27 14:41:48 +0800

    初版

--- dataPlatform-common/src/main/java/com/cqie/util/file/ReadFile.java
+++ dataPlatform-common/src/main/java/com/cqie/util/file/ReadFile.java
@@ -3,8 +3,14 @@ package com.cqie.util.file;
 import cn.afterturn.easypoi.excel.ExcelImportUtil;
 import cn.afterturn.easypoi.excel.entity.ImportParams;
 import org.apache.poi.ss.formula.functions.T;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileInputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 /**
@@ -18,7 +24,16 @@ public class ReadFile {
     public static <T> List<T> readWrite(String path, Class<?> clazz) throws Exception {
         FileInputStream fileInputStream = new FileInputStream(path);
         ImportParams importParams = new ImportParams();
-        importParams.setHeadRows(2);
+
+        Workbook workbook = new XSSFWorkbook(Files.newInputStream(Paths.get(path)));
+        Sheet sheet = workbook.getSheetAt(0);
+        for (int i=0;i<sheet.getLastRowNum();i++){
+            Row row = sheet.getRow(i);
+            if(row!=null&&row.getRowStyle()!=null){
+                importParams.setHeadRows(i+1);
+            }
+        }
+
         importParams.setTitleRows(0);
         List<T> res = ExcelImportUtil.importExcel(fileInputStream, clazz, importParams);
         fileInputStream.close();
